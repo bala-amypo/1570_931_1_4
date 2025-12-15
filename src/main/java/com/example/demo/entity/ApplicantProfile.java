42fb3be90d677de93c82ec495911b1e9f75859b3
bala-amypo <bala@amypo.in>
2025-12-15 16:25:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApplicantProfile.java
+++ src/main/java/com/example/demo/entity/ApplicantProfile.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class ApplicantProfile {
@@ -8,5 +9,115 @@ public class ApplicantProfile {
     private String fullName;
     private LocalDate dateOfBirth;
     private String employmentType;
-    private
+    private BigDecimal monthlyIncome;
+    private BigDecimal existingEmiObligations;
+    private int creditScore;
+    private int internalRiskScore;
+    private String country;
+    private boolean active;
+
+    public ApplicantProfile() {}
+
+    public ApplicantProfile(User user, String fullName, LocalDate dateOfBirth, String employmentType, BigDecimal monthlyIncome, BigDecimal existingEmiObligations, int creditScore, int internalRiskScore, String country, boolean active) {
+        this.user = user;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.employmentType = employmentType;
+        this.monthlyIncome = monthlyIncome;
+        this.existingEmiObligations = existingEmiObligations;
+        this.creditScore = creditScore;
+        this.internalRiskScore = internalRiskScore;
+        this.country = country;
+        this.active = active;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public void setMonthlyIncome(BigDecimal monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public void setExistingEmiObligations(BigDecimal existingEmiObligations) {
+        this.existingEmiObligations = existingEmiObligations;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public void setInternalRiskScore(int internalRiskScore) {
+        this.internalRiskScore = internalRiskScore;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public BigDecimal getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public BigDecimal getExistingEmiObligations() {
+        return existingEmiObligations;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public int getInternalRiskScore() {
+        return internalRiskScore;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    
 }
---
+++ src/main/java/com/example/demo/entity/LoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+public class LoanApplication {
+    private long id;
+    private ApplicantProfile applicant;
+    private 
+    private
+    private
+    private
+    private
+    private
+    private
+}
