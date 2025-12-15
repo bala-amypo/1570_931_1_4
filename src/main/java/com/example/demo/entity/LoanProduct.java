23d55dfdc8e1e306bef1c45fca3b8107c94bd635
bala-amypo <bala@amypo.in>
2025-12-15 16:15:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoanProduct.java
+++ src/main/java/com/example/demo/entity/LoanProduct.java
@@ -1,6 +1,34 @@
 package com.example.demo.entity;
 
+import java.math.BigDecimal;
+
 public class LoanProduct {
     private long id;
+    private String productCode;
+    private String productName;
+    private String loanType;
+    private BigDecimal minAmount;
+    private BigDecimal maxAmount;
+    private int minTenureMonths;
+    private int maxTenureMonths;
+    private double annualInterestRatePercent;
+    private int baseRiskWeight;
+
+    public LoanProduct() {}
+
+    public LoanProduct(String productCode, String productName, String loanType, BigDecimal minAmount,
+            BigDecimal maxAmount, int minTenureMonths, int maxTenureMonths, double annualInterestRatePercent,
+            int baseRiskWeight) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.loanType = loanType;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+        this.minTenureMonths = minTenureMonths;
+        this.maxTenureMonths = maxTenureMonths;
+        this.annualInterestRatePercent = annualInterestRatePercent;
+        this.baseRiskWeight = baseRiskWeight;
+    }
+
     
 }
